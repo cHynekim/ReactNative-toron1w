71d5e7b331f0d6cc9c77ca1e18010c0574486e35
cHynekim <liaco819@naver.com>
2023-11-17 15:58:33 +0900

    171123_native_mypageUI

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import StartIndex from './templates/startIndex';
+import Mypage from './templates/mypage';
 
 export default function App() {
   return (
     <View>
       <StatusBar style="auto"></StatusBar>
-      <StartIndex></StartIndex>
+      <Mypage></Mypage>
     </View>
   );
 }
